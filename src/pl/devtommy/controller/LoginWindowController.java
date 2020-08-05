fe5db7e5068dcb8846c0083c19429c6b70a10a72
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2020-08-05 19:48:41 +0200

    Add Login Window Controller with button test action

--- src/pl/devtommy/controller/LoginWindowController.java
+++ src/pl/devtommy/controller/LoginWindowController.java
@@ -1,2 +1,20 @@
-package pl.devtommy.controller;public class LoginWindowController {
+package pl.devtommy.controller;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class LoginWindowController {
+    @FXML
+    private TextField emailAddressField;
+
+    @FXML
+    private TextField passwordFileld;
+
+    @FXML
+    private Label errorLabel;
+
+    @FXML
+    void loginButtonAction() {
+        System.out.println("Button click!");
+    }
 }
