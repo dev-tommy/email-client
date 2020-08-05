3c3d70a6715ae7b6586734237b6019875c8babe2
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2020-08-05 20:15:28 +0200

    Add Main Window base Controller

--- src/pl/devtommy/controller/MainWindowController.java
+++ src/pl/devtommy/controller/MainWindowController.java
@@ -1,2 +1,25 @@
-package pl.devtommy.controller;public class MainWindowController {
+package pl.devtommy.controller;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TreeView;
+import javafx.scene.web.WebView;
+
+public class MainWindowController {
+
+    @FXML
+    private TreeView<?> emailsTreeView;
+
+    @FXML
+    private TableView<?> emailsTableView;
+
+    @FXML
+    private WebView emailWebView;
+
+    @FXML
+    void optionsAction() {
+
+    }
+
 }
