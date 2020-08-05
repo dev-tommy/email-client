a27e5c62bf277ed78f2c5871e7cb9013c1d7d99b
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2020-08-05 20:53:16 +0200

    Add showLoginWindow()

--- src/pl/devtommy/view/ViewFactory.java
+++ src/pl/devtommy/view/ViewFactory.java
@@ -1,2 +1,16 @@
-package pl.devtommy.view;public class ViewFactory {
+package pl.devtommy.view;
+
+import pl.devtommy.EmailManager;
+
+public class ViewFactory {
+    private EmailManager emailManager;
+
+    public ViewFactory(EmailManager emailManager) {
+        this.emailManager = emailManager;
+    }
+
+    public void showLoginWindow()
+    {
+        System.out.println("Show login window");
+    }
 }
