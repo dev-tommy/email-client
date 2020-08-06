f6e68f6b4b6cce2523537aedf3aaaec71022eced
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2020-08-06 19:47:23 +0200

    Add fieldsAreValid() method

--- src/pl/devtommy/controller/LoginWindowController.java
+++ src/pl/devtommy/controller/LoginWindowController.java
@@ -4,6 +4,8 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import pl.devtommy.EmailManager;
+import pl.devtommy.controller.services.LoginService;
+import pl.devtommy.model.EmailAccount;
 import pl.devtommy.view.ViewFactory;
 
 public class LoginWindowController extends BaseController {
@@ -22,9 +24,34 @@ public class LoginWindowController extends BaseController {
 
     @FXML
     void loginButtonAction() {
-        System.out.println("loginButtonAction!!");
-        viewFactory.showMainWindow();
-        Stage stage = (Stage) errorLabel.getScene().getWindow();
-        viewFactory.closeStage(stage);
+        System.out.println("loginButtonAction");
+        if(fieldsAreValid()){
+            EmailAccount emailAccount = new EmailAccount(emailAddressField.getText(), passwordField.getText());
+            LoginService loginService = new LoginService(emailAccount, emailManager);
+            EmailLoginResult emailLoginResult = loginService.login();
+
+            switch (emailLoginResult) {
+                case SUCCESS:
+                    System.out.println("login succesfull " + emailAccount);
+                    return;
+            }
+        }
+
+        //viewFactory.showMainWindow();
+        //Stage stage = (Stage) errorLabel.getScene().getWindow();
+        //viewFactory.closeStage(stage);
+    }
+
+    private boolean fieldsAreValid() {
+        if (emailAddressField.getText().isEmpty()) {
+            errorLabel.setText("Please fill email");
+            return false;
+        }
+
+        if (passwordField.getText().isEmpty()) {
+            errorLabel.setText("Please fill password");
+            return false;
+        }
+        return true;
     }
 }
