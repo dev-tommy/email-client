2745b81b1a587681eda84d67a54a368366ea7dd5
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2020-08-06 07:58:26 +0200

    Add closeStage()

--- src/pl/devtommy/view/ViewFactory.java
+++ src/pl/devtommy/view/ViewFactory.java
@@ -51,4 +51,8 @@ public class ViewFactory {
         stage.setScene(scene);
         stage.show();
     }
+
+    public void closeStage(Stage stageToClose) {
+        stageToClose.close();
+    }
 }
