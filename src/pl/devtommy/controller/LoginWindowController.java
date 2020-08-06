fdca5c77f85d54fe3f67dfc2092e23af2ec3c391
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2020-08-06 07:57:57 +0200

    Add close login window after show main window

--- src/pl/devtommy/controller/LoginWindowController.java
+++ src/pl/devtommy/controller/LoginWindowController.java
@@ -2,6 +2,7 @@ package pl.devtommy.controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import pl.devtommy.EmailManager;
 import pl.devtommy.view.ViewFactory;
 
@@ -23,5 +24,7 @@ public class LoginWindowController extends BaseController {
     void loginButtonAction() {
         System.out.println("loginButtonAction!!");
         viewFactory.showMainWindow();
+        Stage stage = (Stage) errorLabel.getScene().getWindow();
+        viewFactory.closeStage(stage);
     }
 }
