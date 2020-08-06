235ed5f87e08f6cdd645ded9e1a2b59b769c9394
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2020-08-06 19:12:57 +0200

    Add login method to email account

---
+++ src/pl/devtommy/controller/services/LoginService.java
@@ -0,0 +1,49 @@
+package pl.devtommy.controller.services;
+
+import pl.devtommy.EmailManager;
+import pl.devtommy.controller.EmailLoginResult;
+import pl.devtommy.model.EmailAccount;
+
+import javax.mail.*;
+
+public class LoginService {
+    EmailAccount emailAccount;
+    EmailManager emailManager;
+
+    public LoginService(EmailAccount emailAccount, EmailManager emailManager) {
+        this.emailAccount = emailAccount;
+        this.emailManager = emailManager;
+    }
+
+    private EmailLoginResult login() {
+        Authenticator authenticator = new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return  new PasswordAuthentication(emailAccount.getAddress(), emailAccount.getPassword());
+            }
+        };
+
+        try {
+            Session session = Session.getInstance(emailAccount.getProperties(), authenticator);
+            Store store = session.getStore("imaps");
+            store.connect(emailAccount.getProperties().getProperty("incomingHost"), emailAccount.getAddress(),
+                    emailAccount.getPassword());
+            emailAccount.setStore(store);
+
+        } catch (NoSuchProviderException e){
+            e.printStackTrace();
+            return EmailLoginResult.FAILED_BY_UNEXPECTED_ERROR;
+        } catch (AuthenticationFailedException e){
+            e.printStackTrace();
+            return EmailLoginResult.FAILED_BY_CREDENTIALS;
+        } catch (MessagingException e){
+            e.printStackTrace();
+            return EmailLoginResult.FAILED_BY_UNEXPECTED_ERROR;
+        } catch (Exception e){
+            e.printStackTrace();
+            return EmailLoginResult.FAILED_BY_UNEXPECTED_ERROR;
+        }
+        return EmailLoginResult.SUCCESS;
+    }
+
+}
