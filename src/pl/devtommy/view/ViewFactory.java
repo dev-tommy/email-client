8327678843757e75897ac6b7c963609eb640a262
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2020-08-06 07:31:11 +0200

    Show Login Window with controller name arg

--- src/pl/devtommy/view/ViewFactory.java
+++ src/pl/devtommy/view/ViewFactory.java
@@ -1,6 +1,14 @@
 package pl.devtommy.view;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import pl.devtommy.EmailManager;
+import pl.devtommy.controller.BaseController;
+import pl.devtommy.controller.LoginWindowController;
+
+import java.io.IOException;
 
 public class ViewFactory {
     private EmailManager emailManager;
@@ -12,5 +20,22 @@ public class ViewFactory {
     public void showLoginWindow()
     {
         System.out.println("Show login window");
+
+        BaseController controller = new LoginWindowController(emailManager, this, "LoginWindow.fxml");
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(controller.getFxmlName()));
+        fxmlLoader.setController(controller);
+        Parent parent;
+        try {
+            parent = fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        Scene scene = new Scene(parent);
+        Stage stage = new Stage();
+
+        stage.setScene(scene);
+        stage.show();
     }
 }
