1de9ebfda42f226e5ab8fcdce77ca646c00cd273
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2020-08-08 15:28:32 +0200

    Add addEmailAccount() to TreeItem

--- src/pl/devtommy/EmailManager.java
+++ src/pl/devtommy/EmailManager.java
@@ -1,4 +1,19 @@
 package pl.devtommy;
 
+import javafx.scene.control.TreeItem;
+import pl.devtommy.model.EmailAccount;
+
 public class EmailManager {
+    private TreeItem<String> foldersRoot = new TreeItem<String>("");
+
+    public TreeItem<String> getFoldersRoot() {
+        return foldersRoot;
+    }
+
+    public void addEmailAccount(EmailAccount emailAccount){
+        TreeItem<String> treeItem = new TreeItem<String>(emailAccount.getAddress());
+        treeItem.setExpanded(true);
+            treeItem.getChildren().add(new TreeItem<String>("Inbox"));
+        foldersRoot.getChildren().add(treeItem);
+    }
 }
