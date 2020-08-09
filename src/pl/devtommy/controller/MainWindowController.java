b41eb80e7198bfa9aefd38a3646c5b14a21ee1af
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2020-08-09 16:26:12 +0200

    Update types of TableColumns and show selected folder emails in TableView

--- src/pl/devtommy/controller/MainWindowController.java
+++ src/pl/devtommy/controller/MainWindowController.java
@@ -3,13 +3,18 @@ package pl.devtommy.controller;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TreeView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.web.WebView;
 import pl.devtommy.EmailManager;
+import pl.devtommy.model.EmailMessage;
+import pl.devtommy.model.EmailTreeItem;
 import pl.devtommy.view.ViewFactory;
 
 import java.net.URL;
+import java.util.Date;
 import java.util.ResourceBundle;
 
 public class MainWindowController extends BaseController implements Initializable {
@@ -18,7 +23,22 @@ public class MainWindowController extends BaseController implements Initializabl
     private TreeView<String> emailsTreeView;
 
     @FXML
-    private TableView<?> emailsTableView;
+    private TableView<EmailMessage> emailsTableView;
+
+    @FXML
+    private TableColumn<EmailMessage, String> senderCol;
+
+    @FXML
+    private TableColumn<EmailMessage, String> subjectCol;
+
+    @FXML
+    private TableColumn<EmailMessage, String> recipientCol;
+
+    @FXML
+    private TableColumn<EmailMessage, Integer> sizeCol;
+
+    @FXML
+    private TableColumn<EmailMessage, Date> dateCol;
 
     @FXML
     private WebView emailWebView;
@@ -35,6 +55,26 @@ public class MainWindowController extends BaseController implements Initializabl
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         setUpEmailsTreeView();
+        setUpEmailsTableView();
+        setUpFolderSelection();
+    }
+
+    private void setUpFolderSelection() {
+        emailsTreeView.setOnMouseClicked(event -> {
+            EmailTreeItem<String> item = (EmailTreeItem<String>) emailsTreeView.getSelectionModel().getSelectedItem();
+            if (item != null) {
+                emailsTableView.setItems(item.getEmailMessages());
+            }
+        });
+    }
+
+    private void setUpEmailsTableView() {
+        senderCol.setCellValueFactory(new PropertyValueFactory<EmailMessage, String>("sender"));
+        subjectCol.setCellValueFactory(new PropertyValueFactory<EmailMessage, String>("subject"));
+        recipientCol.setCellValueFactory(new PropertyValueFactory<EmailMessage, String>("recipient"));
+        sizeCol.setCellValueFactory(new PropertyValueFactory<EmailMessage, Integer>("size"));
+        dateCol.setCellValueFactory(new PropertyValueFactory<EmailMessage, Date>("date"));
+
     }
 
     private void setUpEmailsTreeView() {
