ea1c4df50887d462f234980359970227b66b59fc
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2020-08-10 18:54:31 +0200

    Change sizeCol<S,T> T type from int to SizeInteger

--- src/pl/devtommy/controller/MainWindowController.java
+++ src/pl/devtommy/controller/MainWindowController.java
@@ -13,6 +13,7 @@ import javafx.util.Callback;
 import pl.devtommy.EmailManager;
 import pl.devtommy.model.EmailMessage;
 import pl.devtommy.model.EmailTreeItem;
+import pl.devtommy.model.SizeInteger;
 import pl.devtommy.view.ViewFactory;
 
 import java.net.URL;
@@ -37,7 +38,7 @@ public class MainWindowController extends BaseController implements Initializabl
     private TableColumn<EmailMessage, String> recipientCol;
 
     @FXML
-    private TableColumn<EmailMessage, Integer> sizeCol;
+    private TableColumn<EmailMessage, SizeInteger> sizeCol;
 
     @FXML
     private TableColumn<EmailMessage, Date> dateCol;
@@ -96,7 +97,7 @@ public class MainWindowController extends BaseController implements Initializabl
         senderCol.setCellValueFactory(new PropertyValueFactory<EmailMessage, String>("sender"));
         subjectCol.setCellValueFactory(new PropertyValueFactory<EmailMessage, String>("subject"));
         recipientCol.setCellValueFactory(new PropertyValueFactory<EmailMessage, String>("recipient"));
-        sizeCol.setCellValueFactory(new PropertyValueFactory<EmailMessage, Integer>("size"));
+        sizeCol.setCellValueFactory(new PropertyValueFactory<EmailMessage, SizeInteger>("size"));
         dateCol.setCellValueFactory(new PropertyValueFactory<EmailMessage, Date>("date"));
 
     }
