46a0c7b2ca9d22a07986a9d17bcb5c76675ceede
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2020-08-11 21:50:08 +0200

    Add setSelectedMessage() setSelectedFolder() and setRead()

--- src/pl/devtommy/EmailManager.java
+++ src/pl/devtommy/EmailManager.java
@@ -4,13 +4,34 @@ import javafx.scene.control.TreeItem;
 import pl.devtommy.controller.services.FetchFoldersService;
 import pl.devtommy.controller.services.FolderUpdaterService;
 import pl.devtommy.model.EmailAccount;
+import pl.devtommy.model.EmailMessage;
 import pl.devtommy.model.EmailTreeItem;
 
+import javax.mail.Flags;
 import javax.mail.Folder;
 import java.util.ArrayList;
 import java.util.List;
 
 public class EmailManager {
+    private EmailMessage selectedMessage;
+    private EmailTreeItem<String> selectedFolder;
+
+    public EmailMessage getSelectedMessage() {
+        return selectedMessage;
+    }
+
+    public void setSelectedMessage(EmailMessage selectedMessage) {
+        this.selectedMessage = selectedMessage;
+    }
+
+    public EmailTreeItem<String> getSelectedFolder() {
+        return selectedFolder;
+    }
+
+    public void setSelectedFolder(EmailTreeItem<String> selectedFolder) {
+        this.selectedFolder = selectedFolder;
+    }
+
     private FolderUpdaterService folderUpdaterService;
     private EmailTreeItem<String> foldersRoot = new EmailTreeItem<String>("");
 
@@ -33,5 +54,15 @@ public class EmailManager {
         FetchFoldersService fetchFoldersService = new FetchFoldersService(emailAccount.getStore(), treeItem, folderList);
         fetchFoldersService.start();
         foldersRoot.getChildren().add(treeItem);
+    }
+
+    public void setRead() {
+        try {
+            selectedMessage.setRead(true);
+            selectedMessage.getMessage().setFlag(Flags.Flag.SEEN, true);
+            selectedFolder.decrementMessageCount();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
