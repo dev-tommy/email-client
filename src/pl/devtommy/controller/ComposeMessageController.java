9b15d27b51d41fc4c83c6fdbfaf2cc7cc61cf5a5
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2020-08-15 14:43:47 +0200

    Add base controller

---
+++ src/pl/devtommy/controller/ComposeMessageController.java
@@ -0,0 +1,34 @@
+package pl.devtommy.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.web.HTMLEditor;
+import pl.devtommy.EmailManager;
+import pl.devtommy.view.ViewFactory;
+
+public class ComposeMessageController extends BaseController{
+
+
+    @FXML
+    private TextField recipientTextField;
+
+    @FXML
+    private TextField subjectTextField;
+
+    @FXML
+    private HTMLEditor htmlEditor;
+
+    @FXML
+    private Label errorLabel;
+
+    @FXML
+    void sendButtonAction() {
+        System.out.println(htmlEditor.getHtmlText());
+        System.out.println("send button action");
+    }
+
+    public ComposeMessageController(EmailManager emailManager, ViewFactory viewFactory, String fxmlName) {
+        super(emailManager, viewFactory, fxmlName);
+    }
+}
