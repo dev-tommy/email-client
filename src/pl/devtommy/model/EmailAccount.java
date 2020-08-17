24d9c6cc4fdd1043b5be2ea1027d3fd16423a804
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2020-08-17 17:29:01 +0200

    Add Session value

--- src/pl/devtommy/model/EmailAccount.java
+++ src/pl/devtommy/model/EmailAccount.java
@@ -1,5 +1,6 @@
 package pl.devtommy.model;
 
+import javax.mail.Session;
 import javax.mail.Store;
 import java.util.Properties;
 
@@ -10,6 +11,15 @@ public class EmailAccount {
     private String password;
     private Properties properties;
     private Store store;
+    private Session session;
+
+    public Session getSession() {
+        return session;
+    }
+
+    public void setSession(Session session) {
+        this.session = session;
+    }
 
     public String getAddress() {
         return address;
