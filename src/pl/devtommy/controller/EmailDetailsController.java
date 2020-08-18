fe684bf6b5fb0f078ece864a41bcf5ee89c01bd3
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2020-08-18 20:27:59 +0200

    Implement Initialize to update email details window and restart messageRendenerService

--- src/pl/devtommy/controller/EmailDetailsController.java
+++ src/pl/devtommy/controller/EmailDetailsController.java
@@ -1,13 +1,19 @@
 package pl.devtommy.controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.web.WebView;
 import pl.devtommy.EmailManager;
+import pl.devtommy.controller.services.MessageRendererService;
+import pl.devtommy.model.EmailMessage;
 import pl.devtommy.view.ViewFactory;
 
-public class EmailDetailsController extends BaseController{
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class EmailDetailsController extends BaseController implements Initializable {
 
     @FXML
     private WebView webView;
@@ -26,5 +32,16 @@ public class EmailDetailsController extends BaseController{
 
     public EmailDetailsController(EmailManager emailManager, ViewFactory viewFactory, String fxmlName) {
         super(emailManager, viewFactory, fxmlName);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        EmailMessage emailMessage = emailManager.getSelectedMessage();
+        subjectLabel.setText(emailMessage.getSubject());
+        senderLabel.setText(emailMessage.getSender());
+
+        MessageRendererService messageRendererService = new MessageRendererService(webView.getEngine());
+        messageRendererService.setEmailMessage(emailMessage);
+        messageRendererService.restart();
     }
 }
