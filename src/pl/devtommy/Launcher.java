53cd9940ddfe9288ed68724df06b990c2e107eef
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2020-08-25 20:24:37 +0200

    Override stop() method and save persistence to file

--- src/pl/devtommy/Launcher.java
+++ src/pl/devtommy/Launcher.java
@@ -5,9 +5,19 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import pl.devtommy.controller.persistence.PersistenceAccess;
+import pl.devtommy.controller.persistence.ValidAccount;
+import pl.devtommy.model.EmailAccount;
 import pl.devtommy.view.ViewFactory;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Launcher extends Application {
+
+    private PersistenceAccess persistenceAccess = new PersistenceAccess();
+    private EmailManager emailManager = new EmailManager();
+
     public static void main(String[] args) {
         launch(args);
     }
@@ -15,7 +25,16 @@ public class Launcher extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        ViewFactory viewFactory = new ViewFactory(new EmailManager());
+        ViewFactory viewFactory = new ViewFactory(emailManager);
         viewFactory.showLoginWindow();
+    }
+
+    @Override
+    public void stop() throws Exception {
+        List<ValidAccount> validAccountList = new ArrayList<ValidAccount>();
+        for (EmailAccount emailAccount: emailManager.getEmailAccounts()) {
+            validAccountList.add(new ValidAccount(emailAccount.getAddress(), emailAccount.getPassword()));
+        }
+        persistenceAccess.saveToPersistence(validAccountList);
     }
 }
