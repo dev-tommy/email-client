efe3c3710c5b9bc09829713eb8aa398fc22f78e2
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2020-08-25 14:50:27 +0200

    Add file to attachments in button action

--- src/pl/devtommy/controller/ComposeMessageController.java
+++ src/pl/devtommy/controller/ComposeMessageController.java
@@ -6,17 +6,22 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.web.HTMLEditor;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import pl.devtommy.EmailManager;
 import pl.devtommy.controller.services.EmailSenderService;
 import pl.devtommy.model.EmailAccount;
 import pl.devtommy.view.ViewFactory;
 
+import java.io.File;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class ComposeMessageController extends BaseController implements Initializable {
 
+    private List<File> attachments = new ArrayList<File>();
 
     @FXML
     private TextField recipientTextField;
@@ -34,12 +39,22 @@ public class ComposeMessageController extends BaseController implements Initiali
     private ChoiceBox<EmailAccount> emailAccountChoice;
 
     @FXML
+    void attachButtonAction() {
+        FileChooser fileChooser = new FileChooser();
+        File selectedFile = fileChooser.showOpenDialog(null);
+        if(selectedFile != null){
+            attachments.add(selectedFile);
+        }
+    }
+
+    @FXML
     void sendButtonAction() {
         EmailSenderService emailSenderService = new EmailSenderService(
                 emailAccountChoice.getValue(),
                 subjectTextField.getText(),
                 recipientTextField.getText(),
-                htmlEditor.getHtmlText()
+                htmlEditor.getHtmlText(),
+                attachments
         );
         emailSenderService.start();
         emailSenderService.setOnSucceeded(e->{
