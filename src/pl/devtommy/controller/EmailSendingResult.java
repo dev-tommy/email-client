52042de9bc93638f19ba0f832c6301fb718e6705
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2020-09-05 15:07:34 +0200

    Add INVALID_RECIPIENT

--- src/pl/devtommy/controller/EmailSendingResult.java
+++ src/pl/devtommy/controller/EmailSendingResult.java
@@ -3,6 +3,7 @@ package pl.devtommy.controller;
 public enum EmailSendingResult {
     SUCCESS,
     NO_RECIPIENT,
+    INVALID_RECIPIENT,
     FAILED_BY_PROVIDER,
     FAILED_BY_UNEXPECTED_ERROR;
 }
